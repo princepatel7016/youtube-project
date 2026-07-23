4f12db432f23c8e17fc224ce532c7719423e1bdd
prince <princepatel27806@gmail.com>
2026-07-23 12:44:23 +0530

    feat: add addComment and updateComment controllers with routes for comment management

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -3,6 +3,7 @@ import { ApiError } from "../utils/apiError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { Comment } from "../models/comment.model.js";
 import { Mongoose } from "mongoose";
+import { verifyjwt } from "../middleware/auth.middleware.js";
 
 const getVideocomment = asynchandler( async (req,res)=>{
     const { videoId } = req.params
@@ -30,8 +31,60 @@ const getVideocomment = asynchandler( async (req,res)=>{
 })
 
 
+const addComment = asynchandler(async (req,res)=>{
+    const {videoId} = req.params
+
+    const { content } = req.body 
+    console.log("content: ", content)
+
+    if(!content?.trim()){
+        throw new ApiError(400,"add content please")
+    }
+
+    const comment = await Comment.create({
+        content:content,
+        video:videoId,
+        owner:req.user._id
+    }
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200,comment,"comment is sussfully")
+    )
+})
+
+
+const updateComment = asynchandler(async (req,res)=>{
+
+    const {commentId} = req.params
+
+    const {content} = req.body
+
+    if(!content){
+        throw new ApiError(400,"not available content")
+    }
+
+    const comment = await Comment.findByIdAndUpdate(
+        commentId,
+        {
+            $set:{
+                content:content
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    return res.status(200).json(
+        new ApiResponse(200,comment,"update comment succfully")
+    )
+})
+
 
 
 export {
     getVideocomment,
+    addComment,
+    updateComment 
 }
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { verifyjwt } from "../middleware/auth.middleware.js";
-import { getVideocomment } from "../controllers/comment.js";
+import { getVideocomment , addComment, updateComment } from "../controllers/comment.js";
 
 const router = Router()
 
 router.route("/getVideocomment/:videoid").get(getVideocomment)
+
+router.route("/comment/:videoid").post(verifyjwt,addComment)
+
+router.route("/updatecomment/:commentId").patch(verifyjwt,updateComment )
 
 
 export default router
