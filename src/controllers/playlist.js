0eea0c711a7144a7c18071708feee2f900569f09
prince <princepatel27806@gmail.com>
2026-07-24 08:21:26 +0530

    feat: add controllers and routes for fetching, adding, removing videos from playlists, and updating playlists

--- src/controllers/playlist.js
+++ src/controllers/playlist.js
@@ -4,6 +4,7 @@ import { ApiResponse } from "../utils/apiResponse.js";
 import { asynchandler } from "../utils/asynchandler.js";
 import { Playlist } from "../models/playlist.model.js";
 import { verifyjwt } from "../middleware/auth.middleware.js";
+import { Video } from "../models/video.model.js";
 
 const createPlaylist = asynchandler(async(req,res)=>{
     const {name , description} = req.body
@@ -45,10 +46,131 @@ const getUserPlaylist = asynchandler(async(req,res)=>{
     )
 })
 
+const getPlaylistById = asynchandler(async(req,res)=>{
+    //2 ,3 playlist me se kis playlist chaiye uske liye
+    const {playlistId} = req.params
 
+    const playlist = await Playlist.findById(
+        playlistId
+    )
 
+    if(!playlist){
+        throw new ApiError(404,"playlist not found")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,playlist,"playlist fetched sussfully")
+    )
+})
+
+const addVideotoPlaylist = asynchandler(async(req,res)=>{
+    const {playlistId , videoId} = req.params
+
+    const video = await Video.findById(
+        videoId
+    )
+
+    if(!video){
+        throw new ApiError(404,"video not found")
+    }
+
+    const addplaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $push:{
+            video:videoId
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    if(!addplaylist){
+        throw new ApiError(404,"playlist not found")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,addplaylist,"video added to playlist sussfully")
+    )
+})
+
+const removevideofromplaylist = asynchandler(async(req,res)=>{
+    const {playlistId , videoId} = req.params
+
+    const video = await Video.findById(
+        videoId
+    )
+
+    if(!video){
+        throw new ApiError(404,"video not found")
+    }
+
+    const playlist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $pull:{
+                video:videoId
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    if(!playlist){
+        throw new ApiError(404,"playlist not found")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,playlist,"video removed from playlist sussfully")
+    )
+})
+
+const updatePlaylist = asynchandler(async(req,res)=>{
+    const {playlistId} = req.params
+
+    if(!playlistId){
+        throw new ApiError(400,"playlistId is required")
+    }
+
+    const {name, description} = req.body
+
+    if(!name?.trim()){
+        throw new ApiError(400,"name is required")
+    }
+
+    if(!description){
+        throw new ApiError(400,"description is required")
+    }
+
+    const playlist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $set:{
+                name:name,
+                description:description
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    if(!playlist){
+        throw new ApiError(404,"playlist not found")
+    }
+
+    return res.status(200).json(
+        new ApiResponse(200,playlist,"playlist updated sussfully")
+    )
+})
 
 export {
     createPlaylist,
-    getUserPlaylist
+    getUserPlaylist,
+    getPlaylistById,
+    addVideotoPlaylist,
+    removevideofromplaylist,
+    updatePlaylist 
 }
--- src/routes/playlist.routes.js
+++ src/routes/playlist.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyjwt } from "../middleware/auth.middleware.js";
-import { createPlaylist ,  getUserPlaylist} from "../controllers/playlist.js";
+import { createPlaylist ,  getUserPlaylist ,getPlaylistById , addVideotoPlaylist , removevideofromplaylist , updatePlaylist } from "../controllers/playlist.js";
 
 const router  = Router()
 
@@ -8,5 +8,13 @@ const router  = Router()
 router.route("/createPlaylist").post(verifyjwt,createPlaylist)
 
 router.route("/getUserPlaylist/:userId").get(verifyjwt, getUserPlaylist)
+
+router.route("/getPlaylistById/:playlistId").get(verifyjwt, getPlaylistById)
+
+router.route("/addPlaylist/:videoId/:playlistId").patch(verifyjwt, addVideotoPlaylist)
+
+router.route("/removePlaylist/:videoId/:playlistId").patch(verifyjwt, removevideofromplaylist)
+
+router.route("/updatePlaylist/:playlistId").patch(verifyjwt, updatePlaylist )
 
 export default router
