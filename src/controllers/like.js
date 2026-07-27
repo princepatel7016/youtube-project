f03e96382168a74284645e13ef9e44a5da26d193
prince <princepatel27806@gmail.com>
2026-07-27 12:47:05 +0530

    feat: add toggle comment like functionality; implement togglecommentlike controller and update routes

--- src/controllers/like.js
+++ src/controllers/like.js
@@ -40,6 +40,44 @@ const togglevideolike = asynchandler(async (req,res)=>{
         )
 })
 
+const togglecommentlike = asynchandler(async(req,res)=>{
+    const {commentId} = req.params
+
+     // Check valid ObjectId
+    if (!commentId) {
+        throw new ApiError(400, "Invalid Comment Id");
+    }
+
+    // Check if already liked
+    const alreadyLiked = await Like.findOne({
+        comment: commentId,
+        likedBy: req.user._id
+    });
+
+    // Unlike
+    if (alreadyLiked) {
+        await Like.findByIdAndDelete(alreadyLiked._id);
+
+        return res.status(200).json(
+            new ApiResponse(200,{},"Comment unliked successfully"
+            )
+        );
+    }
+
+    // Like
+    await Like.create({
+        comment: commentId,
+        likedBy: req.user._id
+    });
+
+    return res.status(200).json(
+        new ApiResponse(200,{},"Comment liked successfully"
+        )
+    );
+});
+
+
 export {
-    togglevideolike
+    togglevideolike,
+    togglecommentlike
 }
--- src/routes/like.routes.js
+++ src/routes/like.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { verifyjwt } from "../middleware/auth.middleware.js";
-import { togglevideolike } from "../controllers/like.js";
+import { togglevideolike , togglecommentlike } from "../controllers/like.js";
 
 
 const router = Router()
 
 router.route("/togglevideolike/v/:videoId").post(verifyjwt,togglevideolike)
+
+router.route("/togglecommentlike/v/:commentId").post(verifyjwt,togglecommentlike)
 
 
 export default router
